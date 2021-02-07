cd4f1fc70541a8645c06b5baec2bdf84aa2cb6c4
enhui zhu <zhuen2000@163.com>
2021-02-07 18:24:36 +0000

    update build script

--- server/build/index.js
+++ server/build/index.js
@@ -4,10 +4,19 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 var express_1 = __importDefault(require("express"));
+var mongoose_1 = __importDefault(require("mongoose"));
+var expressConfig_1 = __importDefault(require("./configs/expressConfig"));
+var route_1 = __importDefault(require("./routes/route"));
 var app = express_1.default();
-app.get('/', function (req, res) {
-    res.send('hello, the world');
-});
-app.listen(3000, function () {
-    console.log('listening on port 3000');
+mongoose_1.default
+    .connect("mongodb://" + process.env.DB_SERVERS + "/" + process.env.DB, { useNewUrlParser: true, useUnifiedTopology: true })
+    .catch(function (error) {
+    console.log('connection error', error);
+})
+    .then(function () {
+    expressConfig_1.default(app);
+    new route_1.default(app);
+    app.listen(3000, function () {
+        console.log('listening on port 3000');
+    });
 });
